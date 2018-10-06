04916353b8cbee42e6daff31b85ad3d19545f3f7
kevin tang <kevin26428@gmail.com>
2018-10-05 20:52:56 -0500

    authentication on the database interface

--- js/database.js
+++ js/database.js
@@ -1,23 +1,47 @@
 
 //Initiating functions
 //logging in
-function signIn(){
-	var provider = new firebase.auth.GoogleAuthProvider();
-	firebase.auth().signInWithPopup(provider);
-}
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
-function signOut(){
-	firebase.auth().signOut();
-}
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
-function initFirebaseAuth(){
-	firebase.auth().onAuthStateChanged(authStateObserver);
-}
 
 //datafunctions
 //get information
 function getCurrentUser(){ //returns user info of the current user
-	return firebase.auth().currentUser
+	//return firebase.auth().currentUser
 }
 
 function getUserInfo(id){ //returns user info of the user
@@ -39,3 +63,4 @@ function authStateObserver(user){
 		// Show sign-in button.
 	}	
 }
+
