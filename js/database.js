63efd35c23d7c8c08c2fe300f9a0af8fd567137a
kevin tang <kevin26428@gmail.com>
2018-10-05 19:56:24 -0500

    authentication change function
    -when user signs in or out, local data and ui is updated

--- js/database.js
+++ js/database.js
@@ -1,4 +1,5 @@
 
+//Initiating functions
 //logging in
 function signIn(){
 	var provider = new  firebase.auth.GoogleAuthProvider();
@@ -9,10 +10,43 @@ function signOut(){
 	firebase.auth().signOut();
 }
 
+function initFirebaseAuth(){
+	firebase.auth().onAuthStateChanged(authStateObserver);
+}
 
 //datafunctions
 //get information
-getUserInfo = function(){
+function getUserInfo(){
 
-};
+}
 
+function authStateObserver(user){
+	if (user) { // User is signed in!
+		// Get the signed-in user's profile pic and name.
+		var profilePicUrl = getProfilePicUrl();
+		var userName = getUserName();
+
+		// Set the user's profile pic and name.
+		userPicElement.style.backgroundImage = 'url(' + profilePicUrl + ')';
+		userNameElement.textContent = userName;
+
+		// Show user's profile and sign-out button.
+		userNameElement.removeAttribute('hidden');
+		userPicElement.removeAttribute('hidden');
+		signOutButtonElement.removeAttribute('hidden');
+
+		// Hide sign-in button.
+		signInButtonElement.setAttribute('hidden', 'true');
+
+		// We save the Firebase Messaging Device token and enable notifications.
+		saveMessagingDeviceToken();
+	} else { // User is signed out!
+		// Hide user's profile and sign-out button.
+		userNameElement.setAttribute('hidden', 'true');
+		userPicElement.setAttribute('hidden', 'true');
+		signOutButtonElement.setAttribute('hidden', 'true');
+
+		// Show sign-in button.
+		signInButtonElement.removeAttribute('hidden');
+	}	
+}
