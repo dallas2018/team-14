abec282484e3955a0afb0296c2308afd934aec43
kevin tang <kevin26428@gmail.com>
2018-10-05 20:19:04 -0500

    added temporary html file for database testing

--- js/database.js
+++ js/database.js
@@ -16,37 +16,26 @@ function initFirebaseAuth(){
 
 //datafunctions
 //get information
-function getUserInfo(){
+function getCurrentUser(){ //returns user info of the current user
+	return firebase.auth().currentUser
+}
 
+function getUserInfo(id){ //returns user info of the user
+
+}
+
+function getProductInfo(id){
+	
 }
 
 function authStateObserver(user){
 	if (user) { // User is signed in!
-		// Get the signed-in user's profile pic and name.
-		var profilePicUrl = getProfilePicUrl();
-		var userName = getUserName();
-
-		// Set the user's profile pic and name.
-		userPicElement.style.backgroundImage = 'url(' + profilePicUrl + ')';
-		userNameElement.textContent = userName;
-
-		// Show user's profile and sign-out button.
-		userNameElement.removeAttribute('hidden');
-		userPicElement.removeAttribute('hidden');
-		signOutButtonElement.removeAttribute('hidden');
-
-		// Hide sign-in button.
-		signInButtonElement.setAttribute('hidden', 'true');
 
 		// We save the Firebase Messaging Device token and enable notifications.
 		saveMessagingDeviceToken();
 	} else { // User is signed out!
 		// Hide user's profile and sign-out button.
-		userNameElement.setAttribute('hidden', 'true');
-		userPicElement.setAttribute('hidden', 'true');
-		signOutButtonElement.setAttribute('hidden', 'true');
 
 		// Show sign-in button.
-		signInButtonElement.removeAttribute('hidden');
 	}	
 }
