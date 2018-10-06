4eaf5c2046e2941125e71d6f6ca2d8f31425d99f
kevin tang <kevin26428@gmail.com>
2018-10-06 07:03:23 -0500

    organized html

---
+++ config.js
@@ -0,0 +1,8 @@
+  var config = {
+	apiKey: "",
+	authDomain: "codeforgood-bc151.firebaseapp.com",
+	databaseURL: "https://codeforgood-bc151.firebaseio.com",
+	projectId: "codeforgood-bc151",
+	storageBucket: "codeforgood-bc151.appspot.com",
+	messagingSenderId: "23594376697"
+  };
