0d1be99ef96745e9ea48bb17281856a86c994653
sashbhagav <sashreek_bhagav@hotmail.com>
2018-10-06 06:28:22 -0500

    update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -8,24 +8,51 @@ var seller = document.getElementById("seller");
 var quality = document.getElementById("quality");
 var pricing = document.getElementById("pricing");
 
-function submitClick(){
+function setProductInfo(id, val){
 	var firebaseRef = firebase.database().ref().child("Users").push();
-
+/*
 	var productNameText = productName.value;
 	var itemTagsText = itemTags.value;
 	var charityText = charity.value;
 	var sellerText = seller.value;
 	var qualityText = quality.value;
 	var pricingText = pricing.value;
+ */
+}
 
-    firebaseRef.child("Product Name").set(productNameText);
+function getProductInfo(){
+
+	firebaseRef.child("Product Name").set(productNameText);
     firebaseRef.child("Product Type").set(itemTagsText);
     firebaseRef.child("Charity Name").set(charityText);
     firebaseRef.child("Seller Name").set(sellerText);
     firebaseRef.child("Quality of Product (1-10)").set(qualityText);
     firebaseRef.child("Price of Product").set(pricingText);
-    
-
-
 
 }
+
+function getProduct(id, func){
+	var product = firebase.database().ref("Products").push();
+	product.on("value", func);
+	
+}
+
+function getCharity(data){
+	var charityIn = firebase.database().ref("Charity").push();
+		charityIn.on("value", func);
+}
+
+function setCharity(data){
+	var charityIn = firebase.database().ref("Charity").push();
+	charityIn.set(data);
+}
+
+function product(pid, func){
+	firebase.database().ref('/Products/').limitToLast(20).on("value");
+	
+	var name = firebase.database().ref("Product");
+	var description = firebase.database().ref("Product");
+
+}
+
+
