47576aadc0b7758d54ebe70ed47fbde1367a7c2d
kevin tang <kevin26428@gmail.com>
2018-10-05 21:35:23 -0500

    hiding keys and setting up sign in authentication

--- js/database.js
+++ js/database.js
@@ -1,42 +1,31 @@
 
 //Initiating functions
 //logging in
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+function signIn(){
+	var provider = new firebase.auth.GoogleAuthProvider();
+	
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		var token = result.credential.accessToken;
+		var user = result.user;
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+		console.log(token)
+		console.log(user)
+	}).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
 
+		console.log(error.code)
+		console.log(error.message)
+	});
+}
+
+function signOut(){
+   firebase.auth().signOut().then(function() {
+      console.log('Signout Succesfull')
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
 
 //datafunctions
 //get information
@@ -49,7 +38,7 @@ function getUserInfo(id){ //returns user info of the user
 }
 
 function getProductInfo(id){
-	
+	return firebase.database().ref("UserTable");
 }
 
 function authStateObserver(user){
